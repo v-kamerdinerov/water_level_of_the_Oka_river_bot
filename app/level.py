13ae34953cb3614fd320e0ea7d6e8a6e9ab5bfa5
v-kamerdinerov <anakin174jedi@gmail.com>
2024-04-01 23:16:22 +0300

    Use text instead of class

--- app/level.py
+++ app/level.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
@@ -13,13 +14,28 @@ def check_level():
     soup = BeautifulSoup(res.content, 'html.parser')
     logging.info(f"Parsing html page")
 
-    water_class = soup.find('span', {'class': 'gauge-value'})
-    water_level = water_class.text.strip()
-    logging.info(f"get water level {water_level}")
+    text = soup.text
 
-    delta_class = soup.find('span', {'class': 'font-bold'})
-    delta_level = delta_class.text.strip()
-    logging.info(f"get delta {delta_level}")
+    # Регулярное выражение для поиска строки с заданным шаблоном
+    level_pattern = r"уровень воды в реке Ока по данным гидропоста.*?(\d+) cм над нулем поста"
+    delta_pattern = r"повысился на (\d+) см"
+
+    # Поиск совпадений в тексте
+    level_match = re.search(level_pattern, text, re.IGNORECASE)
+    delta_match = re.search(delta_pattern, text, re.IGNORECASE)
+
+    # Если совпадение найдено, извлекаем число
+    if level_match:
+        water_level = level_match.group(1)
+        logging.info(f"get water level {water_level}")
+    else:
+        logging.error("Строка уровня не найдена")
+
+    if delta_match:
+        delta_level = delta_match.group(1)
+        logging.info(f"get delta {delta_level}")
+    else:
+        logging.error("Строка дельты не найдена")
 
     response = 'Уровень воды в реке: ' + water_level + ' см, ' + ' за последние сутки изменился на ' + delta_level + ' см.'
 
